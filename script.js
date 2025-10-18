e0760c587ffc586a621881cf69afb14db55aa296
Максим Пан <maksimpan@MacBook-Pro-Maksim.local>
2025-10-19 00:08:12 +0900

    begin

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
-const mySwiper = new Swiper('.swiper', {
-        slidesPerView: "auto",
-        loop:true,
-        spaceBetween: 16,
-      breakpoints: {
-        320: {
-          slidesPerView:1.5,
-          spaceBetween:16,
-        },
-        768:{
-            slidesPerView:6,
-            spaceBetween:24,
-            
-        }
-      },
+let swiperInstance;
+
+function initSwiper() {
+  if (window.innerWidth < 768 && !swiperInstance) {
+    swiperInstance = new Swiper('.swiper', {
+      slidesPerView: 1.5,
+      spaceBetween: 16,
+      loop: true,
       pagination: {
-        el: ".swiper-pagination",
+        el: '.swiper-pagination',
         clickable: true,
-      }
+      },
     });
+  }
+
+  else if (window.innerWidth >= 768 && swiperInstance) {
+    swiperInstance.destroy(true, true);
+    swiperInstance = null;
+  }
+}
+
+initSwiper();
+
+window.addEventListener('resize', initSwiper);
+
+const buttonWath = document.getElementById('wath-button');
+
+buttonWath.addEventListener("mouseover", event =>{
+
+  event.target.style.backgroundColor = "#7E7E82;"
+  
+})
